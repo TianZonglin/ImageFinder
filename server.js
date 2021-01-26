3bbbdd75b536ce85e3c6f85ef836fe8ccefbefa0
TianZonglin <1805984583@qq.com>
2021-01-26 04:24:19 +0000

    👾🐵 Checkpoint ./public/script.js:20633449/6053 ./public/style.css:20633449/8773 ./server.js:20633449/6880 ./views/index.html:20633449/4154

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 
+
+
+
+
+
+
 var request = require('urllib-sync').request;
 var xpath = require('xpath');
 var path = require('path');
@@ -80,7 +86,7 @@ app.get("/", (request, response) => {
 });
 
 app.post('/fuckqq', urlencodedParser, function (req, res) {
- 
+    
     var url = req.body.wechat;
     var arr = url.split("/");
     var flen = arr.length-7;
@@ -90,26 +96,26 @@ app.post('/fuckqq', urlencodedParser, function (req, res) {
     for(var i=0;i<flen;i++){
       path += arr[i+7]+"/";
     }
- 
+    
     var upath = "https://cdn.jsdelivr.net/gh/"+name+"/"+base+"/"+path;
-    res.send(resolv(url,upath));
+    var list = resolv(url,upath);
+    res.send(list);
+    if(list.length){
+      db.run("INSERT INTO CList (url,size,ctime,ex1,ex2) VALUES ('"+url+"','"+list.length+"','"+new Date().getTime()+"','"+name+"','"+"https://github.com/"+name+".png"+"')");
+      
+    }
+  
+    
  
 })
 
-app.get('/s', function(req, res){
+app.get('/init', function(req, res){
  
-    var url = req.query.b;
-    var arr = url.split("/");
-    var flen = arr.length-7;
-    var name = arr[3];
-    var base = arr[4];
-    var path = "";
-    for(var i=0;i<flen;i++){
-      path += arr[i+7]+"/";
-    }
-  console.log(path);
-    var upath = "https://cdn.jsdelivr.net/gh/"+name+"/"+base+"/"+path;
-    res.send(resolv(url,upath));
+    db.all("select ex1,ex2,size,url,ctime,count(url),max(ctime) from CList group by ex1;", (err, row) => {
+      res.send(row);
+    });  
+  
+    
 
 })
 
@@ -120,3 +126,47 @@ app.get('/s', function(req, res){
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+
+
+
+
+function format(date){
+    var date = new Date(date);//如果date为13位不需要乘1000
+    var Y = date.getFullYear() + '-';
+    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
+    var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
+    var m = (date.getMinutes() <10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+    var s = (date.getSeconds() <10 ? '0' + date.getSeconds() : date.getSeconds());
+    return Y+M+D+h+m+s;
+}
+
+
+// init sqlite db
+const dbFile = "./.data/sqlite.db";
+const exists = fs.existsSync(dbFile);
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database(dbFile);
+
+ 
+
+if(0){
+  //db.run("DROP TABLE Seeds");
+  db.serialize(() => {db.run("CREATE TABLE CList (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, hot TEXT, size TEXT, ctime TEXT, ex1 TEXT, ex2 TEXT, ex3 TEXT, ex4 TEXT)");});
+}
+if(0){
+  db.all("SELECT count(id) from Seeds", (err, row) => {console.log("count > "+JSON.stringify(row));});
+}
+if(0){
+  db.run("DELETE FROM CList WHERE url='https://github.com/zonelyn/bed'");
+}
+if(0){
+  //db.run("INSERT INTO CList (url,size,ctime) VALUES ('https://github.com/zonelyn/bed','20','"+new Date().getTime()+"')");
+  db.all("select ex1,ex2,size,count(url),max(ctime) from CList group by ex1;", (err, row) => {console.log("count > "+JSON.stringify(row));});  
+}  
+ 
+//console.log(format(new Date().getTime())); 
+  
+
