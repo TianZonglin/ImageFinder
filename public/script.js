129531df68b0778b4c6664fe47d5557ef8d6ea43
TianZonglin <1805984583@qq.com>
2021-01-29 06:40:20 +0000

    🛰🚩 Checkpoint ./public/script.js:20633449/109 ./server.js:20633449/123

--- public/script.js
+++ public/script.js
@@ -27,7 +27,7 @@ function firethehole(wechat,o,x) {
           var honor = facebook.list;
           var redmi = 0;
           $("#folders").html("");
-          if(x==2){$("#folders").append(`<div class="fold" title="${1}">[返回上层]</div>`); }
+          if(x==2&&wechat.split("/").length>5){$("#folders").append(`<div class="fold" title="${wechat.replace("/"+wechat.split("/")[wechat.split("/").length-1],"")}">[返回上层]</div>`); }
           huawei.forEach(function(win){
             setTimeout(function() {      
               $("#folders").append(`<div class="fold" title="${facebook.url+"/"+win}" id="${facebook.url}">${win}</div>`); 
