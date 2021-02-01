96ab5fb1934d53c3835ac0405f7c1b81f83b4588
Glitch (hello-express) <none>
2021-02-01 22:24:12 +0000

    check

--- server.js
+++ server.js
@@ -509,3 +509,5 @@ app.get("/payoff", function(req, res) {
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+ 
